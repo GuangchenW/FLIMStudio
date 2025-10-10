99ba4fb8dad532eb9df7c7559b1fdc4ce9c81491
Guangchen Wang <guangchenwa@gmail.com>
2025-10-10 14:55:12 +0100

    Preparing implementation for calcium analysis pipeline

--- flim_studio/plugins/docks.py
+++ flim_studio/plugins/docks.py
@@ -5,7 +5,6 @@ from .app_shell import PhasorAnalysis
 if TYPE_CHECKING: import napari
 
 def open_phasor_analysis(viewer:Viewer) -> PhasorAnalysis:
-	print("Here")
 	panel = PhasorAnalysis(viewer)
 	viewer.window.add_dock_widget(panel, name="FLIM Studio", area="right")
 	return panel
