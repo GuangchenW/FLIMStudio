2e1d24b64491267377229dd18686aea5495cf331
Guangchen Wang <guangchenwa@gmail.com>
2025-11-19 12:14:27 +0000

    Started re-integrating phasor plot control

--- flim_studio/plugins/phasor/ui/phasor_plot_control.py
+++ flim_studio/plugins/phasor/ui/phasor_plot_control.py
@@ -42,57 +42,33 @@ class PhasorControlPanel(QGroupBox):
 	## ------ UI ------ ##
 	def _build(self) -> None:
 		ctrl_grid = QGridLayout()
-		ctrl_grid.setContentsMargins(5,15,5,5)
+		ctrl_grid.setContentsMargins(5,10,5,5)
 		self.setLayout(ctrl_grid)
 
-		#  --- Left side: filter control
-		# First row: intensity filter
-		min_count_label = QLabel("Min photon count")
-		max_count_label = QLabel("Max photon count")
-		self.min_count = QSpinBox()
-		self.min_count.setRange(0, int(1e9))
-		self.max_count = QSpinBox()
-		self.max_count.setRange(1, int(1e9))
-		self.max_count.setValue(int(1e9))
-		ctrl_grid.addWidget(min_count_label, 1, 1)
-		ctrl_grid.addWidget(self.min_count, 1, 2)
-		ctrl_grid.addWidget(max_count_label, 1, 3)
-		ctrl_grid.addWidget(self.max_count, 1, 4)
-		# Second row: median filter
-		kernel_size_label = QLabel("Median filter size")
-		repetition_label = QLabel("Median filter repetition")
-		self.kernel_size = QSpinBox()
-		self.kernel_size.setRange(1, 99)
-		self.kernel_size.setValue(3)
-		self.repetition = QSpinBox()
-		self.repetition.setRange(0, 99)
-		ctrl_grid.addWidget(kernel_size_label, 2, 1)
-		ctrl_grid.addWidget(self.kernel_size, 2, 2)
-		ctrl_grid.addWidget(repetition_label, 2, 3)
-		ctrl_grid.addWidget(self.repetition, 2, 4)
-		# Third row: plot mode and parameters
+		# Plot mode 
 		mode_label = QLabel("Plot mode")
 		self.mode_combo_box = QComboBox()
 		self.mode_combo_box.addItem("contour")
 		self.mode_combo_box.addItem("scatter")
 		self.mode_combo_box.addItem("hist2d")
-		ctrl_grid.addWidget(mode_label, 3, 1)
-		ctrl_grid.addWidget(self.mode_combo_box, 3, 2)
+		ctrl_grid.addWidget(mode_label, 0, 0)
+		ctrl_grid.addWidget(self.mode_combo_box, 0, 1)
+		# Color map
 		cmap_label = QLabel("Color map")
 		self.cmap_combo_box = QComboBox()
 		cmap_names = plt.colormaps()
 		for name in cmap_names:
 			self.cmap_combo_box.addItem(name)
 		self.cmap_combo_box.setCurrentText("jet")
-		ctrl_grid.addWidget(cmap_label, 3, 3)
-		ctrl_grid.addWidget(self.cmap_combo_box, 3, 4)
+		ctrl_grid.addWidget(cmap_label, 0, 2)
+		ctrl_grid.addWidget(self.cmap_combo_box, 0, 3)
 		# Last row: Draw button
 		self.btn_draw = QPushButton("Draw")
 		self.btn_draw.clicked.connect(self._on_btn_draw_clicked)
 		self.btn_map = QPushButton("Map ROI")
 		self.btn_map.clicked.connect(self._on_btn_map_clicked)
-		ctrl_grid.addWidget(self.btn_draw, 4, 1, 1, 2)
-		ctrl_grid.addWidget(self.btn_map, 4, 3, 1, 2)
+		ctrl_grid.addWidget(self.btn_draw, 1, 0, 1, 2)
+		ctrl_grid.addWidget(self.btn_map, 1, 2, 1, 2)
 
 		# --- Right side: dataset management
 		# A list widget where user can select dataset(s) to draw on the plot
@@ -107,7 +83,7 @@ class PhasorControlPanel(QGroupBox):
 			list_item.setSelected(True)
 		self.dataset_list.itemSelectionChanged.connect(self._on_selection_changed)
 		self._on_selection_changed()
-		ctrl_grid.addWidget(self.dataset_list, 1, 5, 4, 1)
+		ctrl_grid.addWidget(self.dataset_list, 0, 4, 2, 1)
 
 	## ------ Public API ------ ##
 	def get_selected_datasets(self) -> list["Dataset"]:
@@ -126,10 +102,6 @@ class PhasorControlPanel(QGroupBox):
 		mode: plotting mode
 		"""
 		params = {}
-		params["min_photon_count"] = self.min_count.value()
-		params["max_photon_count"] = self.max_count.value()
-		params["median_filter_size"] = self.kernel_size.value()
-		params["median_filter_repetition"] = self.repetition.value()
 		params["mode"] = self.mode_combo_box.currentText()
 		params["cmap"] = self.cmap_combo_box.currentText()
 		return params
