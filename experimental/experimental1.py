4ac507ab539a86818c4cdec05cb539e52d4d279b
Guangchen Wang <guangchenwa@gmail.com>
2025-12-11 17:08:54 +0000

    Updates to dependencies

--- experimental/experimental1.py
+++ experimental/experimental1.py
@@ -4,51 +4,32 @@ import phasorpy
 from phasorpy.datasets import fetch
 from phasorpy.io import signal_from_imspector_tiff, signal_from_ptu
 from phasorpy.plot import plot_signal_image
-from phasorpy.phasor import phasor_from_signal, phasor_to_signal, phasor_calibrate, phasor_filter_median, phasor_threshold
 from phasorpy.plot import plot_phasor_image, plot_phasor
+
+from flimlib import GCI_marquardt_fitting_engine
 
 import matplotlib.pyplot as plt
 
-image_path = "samples/Srijan.ptu"
-ref_path = "samples/fluorescein2.ptu"
+image_path = "samples/NADH_12.ptu"
+ref_path = "samples/Atto425_b_2.ptu"
 
+reference_signal = signal_from_ptu(ref_path, frame=-1, channel=0)
 signal = signal_from_ptu(image_path, frame=-1, channel=0)
 frequency = signal.attrs['frequency']
-reference_signal = signal_from_ptu(ref_path, frame=-1, channel=1)
 print(reference_signal.attrs['frequency'], frequency)
 print(signal.shape, signal.dtype)
 
-#plot_signal_image(signal, axis='H', xlabel='delay-time (ns)')
-
-mean, real, imag = phasor_from_signal(signal, harmonic=1)
-
-plot_phasor_image(mean, real, imag, title='Sample')
-
-
-# Get phasor from image and reference
-reference_mean, reference_real, reference_imag = phasor_from_signal(reference_signal, harmonic=1)
-plot_phasor_image(reference_mean, reference_real, reference_imag, title='Sample')
-
-# Calibration
-real, imag = phasor_calibrate(
-	real,
-	imag,
-	reference_mean,
-	reference_real,
-	reference_imag,
-	frequency=frequency,
-	lifetime=4.1,
-)
-
-
-# Median filter with size and repeat, just like FLUTE
-mean, real, imag = phasor_filter_median(mean, real, imag, size=3, repeat=2)
-# Intensity threshold
-mean, real, imag = phasor_threshold(mean, real, imag, mean_min=0.05)
-
-plot_phasor(
-    real,
-    imag,
-    frequency=frequency,
-    title='Calibrated, filtered phasor coordinates',
-)
+# signal["H"].diff("H").mean().item()
+print("Before call", flush=True)
+try:
+	result = GCI_marquardt_fitting_engine(
+		0.003999999984016789,
+		[[[1,2,3,4,5,7,100,70,50,43,34,25,20,16,14,12,10,8,8]]],
+		np.zeros((1,1,3), dtype=np.float32)
+	)
+	print("Call successful", flush=True)
+except Exception as e:
+	print("Call failed", flush=True)
+print("Reached")
+print(result is None)
+print(result.param)
